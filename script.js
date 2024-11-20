b8b6633aadbf465af84b307dae1ff735fbce40c1
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2024-11-21 00:05:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let selectitem = document.querySelector("#colorSelect");
+let removebtn = document.querySelector("input");
+
+removebtn.addEventListener("click", () => {
+  let selectedIndex = selectitem.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    let button = selectitem.options[selectedIndex];
+
+    selectitem.remove(selectedIndex);
+  }
+});
